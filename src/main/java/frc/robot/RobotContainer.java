6a32e19ddf4dc25d70d48bd80acf9901c7677359
redplover <redploveryt@gmail.com>
2024-01-16 10:34:36 -0500

    Fixes state machine and adds scoring to RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import com.ctre.phoenix6.Utils;
@@ -14,8 +15,15 @@ import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.TunerConstants;
 import frc.robot.commands.DriveWithJoysticks;
 import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
+import frc.robot.subsystems.scoring.AimerIO;
+import frc.robot.subsystems.scoring.AimerIOSim;
+import frc.robot.subsystems.scoring.ScoringSubsystem;
+import frc.robot.subsystems.scoring.ShooterIO;
+import frc.robot.subsystems.scoring.ShooterIOSim;
 
 public class RobotContainer {
+    ScoringSubsystem scoringSubsystem;
+
     CommandJoystick leftJoystick = new CommandJoystick(0);
     CommandJoystick rightJoystick = new CommandJoystick(1);
     CommandXboxController controller = new CommandXboxController(2);
@@ -40,6 +48,20 @@ public class RobotContainer {
 
         rightJoystick.button(2)
                 .whileTrue(new InstantCommand(() -> drivetrain.seedFieldRelative()));
+        controller.a().whileTrue(new InstantCommand(() ->
+            scoringSubsystem.setAction(ScoringSubsystem.ScoringAction.INTAKE)));
+
+        controller.b().whileTrue(new InstantCommand(() ->
+            scoringSubsystem.setAction(ScoringSubsystem.ScoringAction.AIM)));
+        
+        controller.x().whileTrue(new InstantCommand(() ->
+            scoringSubsystem.setAction(ScoringSubsystem.ScoringAction.SHOOT)));
+
+        controller.y().whileTrue(new InstantCommand(() ->
+            scoringSubsystem.setAction(ScoringSubsystem.ScoringAction.ABORT)));
+
+        controller.back().whileTrue(new InstantCommand(() ->
+            scoringSubsystem.setAction(ScoringSubsystem.ScoringAction.ENDGAME)));
     }
 
     private void configureModes() {
@@ -50,5 +72,15 @@ public class RobotContainer {
             drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
         }
         drivetrain.registerTelemetry(driveTelemetry::telemeterize);
+
+        switch (Constants.currentMode) {
+            case REAL:
+                scoringSubsystem = new ScoringSubsystem(new ShooterIOSim(), new AimerIOSim());
+                break;
+            case SIM:
+                scoringSubsystem = new ScoringSubsystem(new ShooterIOSim(), new AimerIOSim());
+                break;
+            case REPLAY:
+        }
     }
 }
