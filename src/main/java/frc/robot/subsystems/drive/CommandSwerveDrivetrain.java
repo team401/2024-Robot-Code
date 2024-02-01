a7282fc609364907725b81acb12bc4bb80864036
Minh Nguyen <minhnguyen@mcps.org>
2024-02-01 18:23:12 -0500

    YAY follows red+blue paths, unsure what heading looks like, robot does not start out at correct position

--- src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
@@ -93,7 +93,8 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
                 this::getCurrentRobotChassisSpeeds,
                 (speeds) ->
                         this.setGoalChassisSpeeds(
-                                speeds, true, true), // Consumer of ChassisSpeeds to drive the robot
+                                speeds, false,
+                                true), // Consumer of ChassisSpeeds to drive the robot
                 new HolonomicPathFollowerConfig(
                         new PIDConstants(10, 0, 0),
                         new PIDConstants(10, 0, 0),
