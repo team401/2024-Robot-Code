bdff9b1e2314719e2ec03ebec89e64409640c581
Minh Nguyen <minhnguyen@mcps.org>
2024-02-03 19:31:39 -0500

    fixed format(?)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -54,11 +53,9 @@ public class RobotContainer {
 
     Telemetry driveTelemetry = new Telemetry(DriveConstants.MaxSpeedMetPerSec);
 
-
     SendableChooser<String> autoChooser = new SendableChooser<String>();
 
     EndgameSubsystem endgameSubsystem;
-
 
     public RobotContainer() {
         configureBindings();
