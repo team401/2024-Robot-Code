15828eb5c8eb62e0cd8a045a452c593ad21f71cc
Minh Nguyen <minhnguyen@mcps.org>
2024-02-12 22:03:45 -0500

    fixed merge things

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,9 +48,9 @@ public final class Constants {
     public static final class FeatureFlags {
         public static final boolean runVision = false;
 
-        public static final boolean runIntake = false;
+        public static final boolean runIntake = true;
         public static final boolean runScoring = true;
-        public static final boolean runEndgame = true;
+        public static final boolean runEndgame = false;
         public static final boolean runDrive = true;
 
         public static final boolean enableLEDS = false;
