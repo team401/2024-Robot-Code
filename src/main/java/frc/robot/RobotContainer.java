945b4d4a130cd1e8a8ff814e13ec031b9045e3ec
charlesmackenzie <charlesmackenzie@mcps.org>
2024-02-22 01:03:35 -0500

    Register drive as a subsystem on construction to assure it moves in auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -684,6 +685,7 @@ public class RobotContainer {
                 "Wait Scoring",
                 new InstantCommand(
                         () -> scoringSubsystem.setAction(ScoringSubsystem.ScoringAction.WAIT)));
+        NamedCommands.registerCommand("Intake Note", Commands.none());
     }
 
     public void teleopInit() {
