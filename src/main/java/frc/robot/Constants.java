55d02ee3741e0abfee7e3f1f9329203e0f81303e
charlesmackenzie <charlesmackenzie@mcps.org>
2024-03-03 08:12:18 -0500

    Automated commit at 03/03/2024 08:12:17

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,9 +48,9 @@ public final class Constants {
     public static final class FeatureFlags {
         public static final boolean runVision = true;
 
-        public static final boolean runIntake = true;
-        public static final boolean runScoring = true;
-        public static final boolean runEndgame = true;
+        public static final boolean runIntake = false;
+        public static final boolean runScoring = false;
+        public static final boolean runEndgame = false;
         public static final boolean runDrive = true;
 
         public static final boolean enableLEDS = false;
@@ -104,13 +104,13 @@ public final class Constants {
 
         public static final Rotation2d blueUpHeading = Rotation2d.fromRadians(0.0);
         public static final Rotation2d blueDownHeading = Rotation2d.fromRadians(Math.PI);
-        public static final Rotation2d blueLeftHeading = Rotation2d.fromRadians(-Math.PI / 2.0);
-        public static final Rotation2d blueRightHeading = Rotation2d.fromRadians(Math.PI / 2.0);
+        public static final Rotation2d blueLeftHeading = Rotation2d.fromRadians(Math.PI / 2.0);
+        public static final Rotation2d blueRightHeading = Rotation2d.fromRadians(-Math.PI / 2.0);
 
         public static final Rotation2d redUpHeading = Rotation2d.fromRadians(Math.PI);
         public static final Rotation2d redDownHeading = Rotation2d.fromRadians(0.0);
-        public static final Rotation2d redLeftHeading = Rotation2d.fromRadians(Math.PI / 2.0);
-        public static final Rotation2d redRightHeading = Rotation2d.fromRadians(-Math.PI / 2.0);
+        public static final Rotation2d redLeftHeading = Rotation2d.fromRadians(-Math.PI / 2.0);
+        public static final Rotation2d redRightHeading = Rotation2d.fromRadians(Math.PI / 2.0);
 
         public static final Rotation2d redSourceHeading =
                 new Rotation2d(Math.PI * 4 / 3); // 60 degrees
